78044ba80caef9ea9e055bd1b909040c2d2c4715
Will Robertson <will@wspr.io>
2017-08-10 23:30:45 +0930

    add .ttf to install

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ fontpath=$texmf/fonts/opentype/fontspec/
 
 mkdir -p $fontpath
 cp -nv $MYPATH/*/*.otf $HOMETEXMF $fontpath
+cp -nv $MYPATH/*/*.ttf $HOMETEXMF $fontpath
 cp -nv $MYPATH/*/*.TTF $HOMETEXMF $fontpath
